dbc222f377673c1cb6a58f17b2bae665de36fb51
Matt F <fiaschettima@gmail.com>
2022-06-23 11:09:08 -0400

    fixed prompt to cycle until right answer is given

--- Develop/script.js
+++ Develop/script.js
@@ -9,31 +9,26 @@ var numbers =[0,1,2,3,4,5,6,7,8,9,];
 function writePassword() {
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
-  var caps = confirm("Would you like the password to contain Capital letters");//slice second half
-  var low= confirm("Would you like the password to contain lowercase letters");//slice first half
-  var spec =confirm("Can the password contain special characters such as !@$&");
-  var nums = confirm("Would you like the password to contain numbers");
-  var passLength = prompt("Choose how many characters the password can contain. (please choose a number 8-128)")
-  if(passLength < 8 || passLength > 128){
+  // var caps = confirm("Would you like the password to contain Capital letters");//slice second half
+  // var low= confirm("Would you like the password to contain lowercase letters");//slice first half
+  // var spec =confirm("Can the password contain special characters such as !@$&");
+  // var nums = confirm("Would you like the password to contain numbers");
+  var passLength = prompt("Choose how many characters the password can contain. (please choose a number 8-128)");
+  while(passLength < 8 || passLength > 128){
     alert("Password is not within the acceptable amount of characters");
     passLength = prompt("Enter a number greater than 8 and less than 128");
   }
 
   function generatePassword(caps, low, spec, passLength){
-    
+
   }
-  // passwordText.value = password;
+  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-for (password <= passLength){
-
-}
 
 
 
